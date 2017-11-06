e3925ff6ab6bc5d085d3ccc082f0bdf2e70db916
lpmatiutis <lpthemaster@hotmail.com>
2017-11-06 15:40:18 -0300

    Actualizacion 1.0

---
+++ TurismoWeb/TurismoWeb/Controllers/HomeController.vb
@@ -0,0 +1,14 @@
+﻿Imports System.Web.Mvc
+Imports ClasesRestaurante
+Namespace Controllers
+    Public Class HomeController
+        Inherits Controller
+
+        ' GET: Home
+        Function Index() As ActionResult
+            'Establecer la conexión a la base de datos
+            Util.inicializaSesion("M205-03", "Restaurante", "sa", "@lumno123")
+            Return View()
+        End Function
+    End Class
+End Namespace
